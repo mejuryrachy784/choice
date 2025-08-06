77fb9a1f79dc59ae208a3cd66babf72fcabf577a
mejuryrachy784 <mejuryzvarevashe7@gmail.com>
2025-08-06 12:23:46 +0200

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:5001', // Proxy requests to your Express backend
